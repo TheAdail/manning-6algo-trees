1360cc621cc3b7b4b2325b90664918246f52cbf8
TheAdail <adail.retamal@gmail.com>
2022-02-25 21:29:19 +1100

    Milestone 7

---
+++ Binary_Node_7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Binary_Node_7
+{
+    class Program
+    {
+
+        static void PrintList(string header, List<SortedBinaryNode<int>> list)
+        {
+            Console.Write($"{header+":",-15}");
+            foreach (SortedBinaryNode<int> node in list)
+                Console.Write(node.Value + " ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorted Binary Tree\n");
+            Console.WriteLine("Enter: 'a n' to insert value 'n'; 'f n' to find value 'n'; 'q' to quit\n");
+
+            SortedBinaryNode<int> root = new SortedBinaryNode<int>(-1);
+
+            string input = " ";
+
+            while (input != "q")
+            {
+                Console.Write(">");
+                input = Console.ReadLine();
+
+                try
+                {
+                    switch ((input + " ").ToLower()[0])
+                    {
+                        case 'a':
+                            root.AddNode(new SortedBinaryNode<int>(Convert.ToInt32(input[2..].Trim())));
+                            PrintList("Inorder", root.TraverseInorder());
+                            break;
+
+                        case 'f':
+                            if (root.FindNode(Convert.ToInt32(input[2..].Trim())) == null)
+                                Console.WriteLine("Value is not in the tree");
+                            else
+                                Console.WriteLine("Value found");
+                            break;
+
+                        case 'q':
+                            input = "q";
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nError: {ex.Message}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Binary_Node_7/SortedBinaryNode.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Node_7
+{
+    public class SortedBinaryNode<T> where T : IComparable<T>
+    {
+        internal static string Indentation = "  ";
+        internal T Value;
+        internal SortedBinaryNode<T> LeftChild, RightChild;
+
+        public SortedBinaryNode(T Value, SortedBinaryNode<T> Left = null, SortedBinaryNode<T> Right = null)
+        {
+            this.Value = Value;
+            LeftChild = Left;
+            RightChild = Right;
+        }
+
+        public void AddLeft(SortedBinaryNode<T> child)
+        {
+            LeftChild = child;
+        }
+
+        public void AddRight(SortedBinaryNode<T> child)
+        {
+            RightChild = child;
+        }
+
+        public String ValueOrNone(SortedBinaryNode<T> node, int level)
+        {
+            return node == null ? "None" : node.ToString(level);
+        }
+
+        public override String ToString()
+        {
+            return this.ToString(0);
+        }
+
+        public string ToString(int level)
+        {
+            string baseIndentation = level > 0 ? new StringBuilder("").Insert(0, Indentation, level).ToString() : "";
+            string result = $"{baseIndentation}{this.Value}:\n";
+            if(LeftChild != null || RightChild != null)
+            {
+                baseIndentation += Indentation;
+                if (LeftChild == null)
+                    result += $"{baseIndentation}None\n";
+                else
+                    result += LeftChild.ToString(level + 1);
+                if (RightChild == null)
+                    result += $"{baseIndentation}None\n";
+                else
+                    result += RightChild.ToString(level + 1);
+            }
+            return result;
+        }
+
+        public void AddNode(SortedBinaryNode<T> node)
+        {
+            if(node.Value.CompareTo(Value) < 0)
+            {
+                if(LeftChild == null)
+                {
+                    AddLeft(node);
+                }
+                else
+                {
+                    LeftChild.AddNode(node);
+                }
+            }
+            else
+            if (node.Value.CompareTo(Value) > 0)
+            {
+                if (RightChild == null)
+                {
+                    AddRight(node);
+                }
+                else
+                {
+                    RightChild.AddNode(node);
+                }
+            }
+        }
+
+        public SortedBinaryNode<T> FindNode(T value)
+        {
+            if (Value.Equals(value))
+                return this;
+
+            SortedBinaryNode<T> result = null;
+
+            if (value.CompareTo(Value) < 0)
+            {
+                if (LeftChild != null)
+                    result = LeftChild.FindNode(value);
+            }
+            else
+            {
+                if (value.CompareTo(Value) > 0)
+                {
+                    if (RightChild != null)
+                        result = RightChild.FindNode(value);
+                }
+            }
+
+            return result;
+        }
+
+        public List<SortedBinaryNode<T>> TraversePreorder()
+        {
+            List<SortedBinaryNode<T>> result = new List<SortedBinaryNode<T>>() { this };
+            if (LeftChild != null)
+                result.AddRange(LeftChild.TraversePreorder());
+            if (RightChild != null)
+                result.AddRange(RightChild.TraversePreorder());
+            return result;
+        }
+
+        public List<SortedBinaryNode<T>> TraverseInorder()
+        {
+            List<SortedBinaryNode<T>> result = new List<SortedBinaryNode<T>>();
+            if (LeftChild != null)
+                result.AddRange(LeftChild.TraverseInorder());
+            result.Add(this);
+            if (RightChild != null)
+                result.AddRange(RightChild.TraverseInorder());
+            return result;
+        }
+
+        public List<SortedBinaryNode<T>> TraversePostorder()
+        {
+            List<SortedBinaryNode<T>> result = new List<SortedBinaryNode<T>>();
+            if (LeftChild != null)
+                result.AddRange(LeftChild.TraversePostorder());
+            if (RightChild != null)
+                result.AddRange(RightChild.TraversePostorder());
+            result.Add(this);
+            return result;
+        }
+
+        public List<SortedBinaryNode<T>> TraverseBreadthFirst(bool addNode = true)
+        {
+            List<SortedBinaryNode<T>> result = new List<SortedBinaryNode<T>>();
+            if (addNode)
+                result.Add(this);
+            if (LeftChild != null)
+                result.Add(LeftChild);
+            if (RightChild != null)
+                result.Add(RightChild);
+            if (LeftChild != null)
+                result.AddRange(LeftChild.TraverseBreadthFirst(false));
+            if (RightChild != null)
+                result.AddRange(RightChild.TraverseBreadthFirst(false));
+            return result;
+        }
+
+    }
+}
